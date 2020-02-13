72390dafd872ce1bbe3a4d24d19fc30edad1cb1e
phoibos <phoibos947@gmail.com>
2020-02-13 19:05:04 +0100

    encore un commit

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
-var urnumber = prompt ("choose a number between 0 and 10")
-var thnumber = Math.floor(Math.random() * Math.floor(11))
-while ( urnumber != thnumber ) {
-   
-    if ( urnumber < thnumber ){
-        urnumber = prompt ("ur number is to low, choose an higher one")
-    }
-   
-    if ( urnumber > thnumber ){
-        urnumber = prompt ("ur number is to high, choose a lower one")
-    }
-    
+// getElement
+var titleElt = document.getElementById("title")
+var descElt = document.getElementById("essais")
+
+// Configuration de la partie 
+var submitElt = document.getElementsByClassName("submit")[0]
+submitElt.addEventListener("click" , changeScene)
+
+function changeScene(event){
+    event.preventDefault()
+    // Création des variables
+    var tableauDelements = document.getElementsByTagName("input")
+    var input1 = tableauDelements[0]
+    var input2 = tableauDelements[1]
+    var input3 = tableauDelements[2]
+
+    var nbEssaiesRestants = input2.value
+    var nbAChercherMaximal = input1.value
+
+    // Création de la nouvelle scène
+    input2.style.display= "none"
+    input1.placeholder= "Entrez un nombre entre 0 et " + nbAChercherMaximal
+    input1.value=""
+    input2.value=""
+    input3.value="Tester !"
+    descElt.style.display= "block"
+    descElt.textContent=" Il vous reste " + nbEssaiesRestants + " essais"
+    titleElt.textContent="Que la partie commence !"
+
+    startGame(nbAChercherMaximal,nbEssaiesRestants)
 }
-alert ("GG mate, it was the right one !!")
+
+function startgame(nbAChercherMaximal, nbEssaiesRestants){
+    
+    // Programme
+    var texte = "Bienvenue dans la partie de guess the number !\nVeuillez taper un nombre entre 0 et " + nbAChercherMaximal
+    var nbAuHasard = Math.floor(Math.random() * Math.floor(parseInt( nbAChercherMaximal , 10 ) + 1))
+
+    for ( nbEssaiesRestants ; nbEssaiesRestants >= 0 ; nbEssaiesRestants-- ) {
+        //var input = prompt(texte)
+        console.info(input == nbAuHasard)
+        if ( input == nbAuHasard ){
+            alert("BRAVO ! Vous avez trouvé le nombre")
+            nbEssaiesRestants = -1
+            isWin = "gagné"
+        } else if ( input > nbAuHasard ) {
+            texte = "Le nombre est moins grand \nIl vous reste " + nbEssaiesRestants + " essaies"
+        } else if ( input < nbAuHasard ) {
+            texte = "Le nombre est plus grand \nIl vous reste " + nbEssaiesRestants + " essaies"
+        } else {
+            alert("erreur")
+        }
+    }
+}
--- script2.js
+++ script2.js
@@ -1,39 +1,63 @@
+// getElement
+var titleElt = document.getElementById("title")
+var descElt = document.getElementById("essais")
+
 // Configuration de la partie 
 var submitElt = document.getElementsByClassName("submit")[0]
-submitElt.addEventListener("click" , startgame)
+submitElt.addEventListener("click" , changeScene)
 
-function startgame(event){
+function changeScene(event){
     event.preventDefault()
-
     // Création des variables
     var tableauDelements = document.getElementsByTagName("input")
-    var nombreMaximumElt = tableauDelements[0]
-    var nombreEssaisElt = tableauDelements[1]
+    var input1 = tableauDelements[0]
+    var input2 = tableauDelements[1]
+    var input3 = tableauDelements[2]
+
+    var nbEssaiesRestants = input2.value
+    var nbAChercherMaximal = input1.value
+
+    // Création de la nouvelle scène
+    input2.style.display= "none"
+    input1.placeholder= "Entrez un nombre entre 0 et " + nbAChercherMaximal
+    input3.value="Tester !"
+    descElt.style.display= "block"
+
+    startGame(nbAChercherMaximal,nbEssaiesRestants)
+}
+
+function startGame(nbAChercherMaximal, nbEssaiesRestants){
     
-
-    var nbEssaiesRestants = nombreEssaisElt.value
-    var nbAChercherMaximal = nombreMaximumElt.value
-
     // Programme
-   
-    var isWin = "perdu"
-    var texte = "Bienvenue dans la partie de guess the number !\nVeuillez taper un nombre entre 0 et " + nbAChercherMaximal
+    var texteTitle = "Que la partie commence !"
+    var texteDesc = " Il vous reste " + nbEssaiesRestants + " essais"
     var nbAuHasard = Math.floor(Math.random() * Math.floor(parseInt( nbAChercherMaximal , 10 ) + 1))
 
     for ( nbEssaiesRestants ; nbEssaiesRestants >= 0 ; nbEssaiesRestants-- ) {
-        var input = prompt(texte)
+        // Modification de scène
+        var tableauDelements = document.getElementsByTagName("input")
+        var input1 = tableauDelements[0]
+        var input2 = tableauDelements[1]
+        var input3 = tableauDelements[2]
+        input1.value = ""
+        input2.value = ""
+        descElt.textContent = texteDesc
+        titleElt.textContent = texteTitle 
+        //var input = prompt(texte)
+        descElt.textContent = texteDesc
+        input = document.getElementsByTagName("input")[0].value
         console.info(input == nbAuHasard)
         if ( input == nbAuHasard ){
             alert("BRAVO ! Vous avez trouvé le nombre")
             nbEssaiesRestants = -1
-            isWin = "gagné"
         } else if ( input > nbAuHasard ) {
-            texte = "Le nombre est moins grand \nIl vous reste " + nbEssaiesRestants + " essaies"
+            texteTitle = "Le nombre est moins grand"
+            texteDesc = "Il vous reste " + nbEssaiesRestants + " essaies"
         } else if ( input < nbAuHasard ) {
-            texte = "Le nombre est plus grand \nIl vous reste " + nbEssaiesRestants + " essaies"
+            texteTitle = "Le nombre est plus grand"
+            texteDesc = "Il vous reste " + nbEssaiesRestants + " essaies"
         } else {
             alert("erreur")
         }
     }
-    alert("C'est la fin, vous avez " + isWin )
 }
