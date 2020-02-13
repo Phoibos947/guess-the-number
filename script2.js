fea00eec4cf1df7b8105e99cdec9a9c9d97a7d41
Mathis Barré <mathis.barre@live.fr>
2020-02-13 23:40:40 +0100

    Le jeu est maintenant fonctionnel

--- script2.js
+++ script2.js
@@ -9,16 +9,17 @@ submitElt.addEventListener("click" , changeScene)
 function changeScene(event){
     event.preventDefault()
     // Création des variables
-    var tableauDelements = document.getElementsByTagName("input")
-    var input1 = tableauDelements[0]
-    var input2 = tableauDelements[1]
-    var input3 = tableauDelements[2]
+    var input1 = document.getElementById("input1")
+    var input2 = document.getElementById("input2")
+    var input3 = document.getElementById("input3")
 
     var nbEssaiesRestants = input2.value
     var nbAChercherMaximal = input1.value
 
     // Création de la nouvelle scène
     input2.style.display= "none"
+    input2.value = ""
+    input1.value =""
     input1.placeholder= "Entrez un nombre entre 0 et " + nbAChercherMaximal
     input3.value="Tester !"
     descElt.style.display= "block"
@@ -29,35 +30,58 @@ function changeScene(event){
 function startGame(nbAChercherMaximal, nbEssaiesRestants) {
     
     // Programme
-    var texteTitle = "Que la partie commence !"
-    var texteDesc = " Il vous reste " + nbEssaiesRestants + " essais"
+    titleElt.textContent = "Que la partie commence !"
+    descElt.textContent= " Il vous reste " + nbEssaiesRestants + " essais"
     var nbAuHasard = Math.floor(Math.random() * Math.floor(parseInt( nbAChercherMaximal , 10 ) + 1))
+    console.log("Nombre au hasard : " + nbAuHasard)
+    var input3 = document.getElementById("input3")
 
-    for ( nbEssaiesRestants ; nbEssaiesRestants >= 0 ; nbEssaiesRestants-- ) {
-        // Modification de scène
-        var tableauDelements = document.getElementsByTagName("input")
-        var input1 = tableauDelements[0]
-        var input2 = tableauDelements[1]
-        var input3 = tableauDelements[2]
-        input1.value = ""
-        input2.value = ""
-        descElt.textContent = texteDesc
-        titleElt.textContent = texteTitle 
-        //var input = prompt(texte)
-        descElt.textContent = texteDesc
-        input = document.getElementsByTagName("input")[0].value
-        console.info(input == nbAuHasard)
-        if ( input == nbAuHasard ){
-            alert("BRAVO ! Vous avez trouvé le nombre")
+    start()
+
+    input3.removeEventListener("click", changeScene)
+    input3.addEventListener("click", tampon)
+
+    function tampon(e){
+        e.preventDefault()
+        start()
+    }
+
+    function start(){
+        // Game
+        var input = parseInt(document.getElementById("input1").value, 10)
+        console.log(input)
+        if ( nbEssaiesRestants <= 0 ){
+            submitElt.removeEventListener("click" , tampon)
+            reset("Vous avez perdus !\nVous referiez bien une petite partie !?")
+        }
+        else if ( input == nbAuHasard ){
+            submitElt.removeEventListener("click" , tampon)
+            reset("Bravo vous avez trouvé le nombre !\nVous referiez bien une petite partie !?")
             nbEssaiesRestants = -1
         } else if ( input > nbAuHasard ) {
-            texteTitle = "Le nombre est moins grand"
-            texteDesc = "Il vous reste " + nbEssaiesRestants + " essaies"
+            titleElt.textContent = "Le nombre est moins grand"
+            descElt.textContent = "Il vous reste " + nbEssaiesRestants + " essaies"
         } else if ( input < nbAuHasard ) {
-            texteTitle = "Le nombre est plus grand"
-            texteDesc = "Il vous reste " + nbEssaiesRestants + " essaies"
+            titleElt.textContent = "Le nombre est plus grand"
+            descElt.textContent = "Il vous reste " + nbEssaiesRestants + " essaies"
         } else {
-            alert("erreur")
+            console.error("Error")
         }
+        nbEssaiesRestants--
+        input1.value = ""
     }
+}
+
+function reset(texte) {
+    submitElt.addEventListener("click" , changeScene)
+    var input1 = document.getElementById("input1")
+    var input2 = document.getElementById("input2")
+    var input3 = document.getElementById("input3")
+    input2.style.display= "block"
+    input2.value = ""
+    input1.value =""
+    input1.placeholder= "Nombre à trouver maximum"
+    input3.value="LANCER LA PARTIE"
+    descElt.style.display= "none"
+    titleElt.textContent= texte
 }
